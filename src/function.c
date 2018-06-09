dbae24965202dbb20432739570d9a925eb5c2af7
tipKarpenko <Carpenko.ilya@yandex.ru>
2018-06-09 15:17:15 +0700

    add groups of words

---
+++ src/function.c
@@ -0,0 +1,16 @@
+#include "function.h"
+
+char groups[13][9] = {  "`123456" ,
+					"7890-="  ,
+					"~!@#$%^" ,
+					"&*()_+"  ,
+					"<>?" 	  , 	 
+					",./\\"	  ,
+					"asdfg",
+					"jkl;'",
+					"qwer",
+					"uiop",
+					"tyur",
+					"zxcv",
+					"bnm",
+				  };
