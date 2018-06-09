613b1db05158ba6aed3e5f27f5a97345720c1792
tipKarpenko <Carpenko.ilya@yandex.ru>
2018-06-09 15:28:36 +0700

    add print menu functions

--- src/function.c
+++ src/function.c
@@ -59,3 +59,47 @@ void create_test_string(char *test_array, int choice)
 	}
 	test_array[i] = '\0';
 }	
+
+void print_time(time_t start_t, time_t end_t, int mistakes)
+{	long int diff;
+	int h,m,s;
+	float wpm;
+
+	diff = end_t - start_t;
+	wpm = ((HSIZE / 5)/(double)diff)*60;
+
+	h = (int)(diff / 3600);
+    diff -= h * 3600;
+	m = (int)(diff / 60);
+	diff -= m * 60;
+	s = (int)diff; 
+	
+	attron(A_REVERSE);
+	mvprintw(STATUSY, STATUSX, "Ошибок : %d Времени затрачено: %d:%d:%d WPM : %.2f    Нажмите кнопку что бы продолжить", mistakes, h, m, s, wpm);
+	attroff(A_REVERSE);
+
+	refresh();
+	getch();
+
+}
+
+void print_in_middle(int startx, int starty, int width, char *string, WINDOW *win)
+{	int length, x, y;
+	float temp;
+	
+	if(win == NULL)
+		win = stdscr;
+	getyx(win, y, x);
+	if(startx != 0)
+		x = startx;
+	if(starty != 0)
+		y = starty;
+	if(width == 0)
+		width = 80;
+
+	length = strlen(string);
+	temp = (width - length)/ 2;
+	x = startx + (int)temp;	
+	mvwprintw(win, y, x, "%s", string);
+	refresh();
+}
